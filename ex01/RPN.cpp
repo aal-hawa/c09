f1a57b0da669e8f85759e495857a3e93e13ea4f2
aal-hawa <aal-hawa@student.42abudhabi.ae>
2025-10-09 16:27:20 +0400

    edit

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -1,5 +1,12 @@
 #include "RPN.hpp"
 
+RPN::RPN() {}
+RPN::RPN(const RPN& other) {(void) other;}
+RPN& RPN::operator=(const RPN& other) {
+    (void) other;
+    return *this;
+}
+RPN::~RPN() {}
 
 int RPN::evaluate(const  std::string& expression)
 {
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -8,7 +8,14 @@
 
 class RPN
 {
+
+
+
     public:
+        RPN();
+        RPN(const RPN& other);
+        RPN& operator=(const RPN& other);
+        ~RPN();
         static int evaluate(const std::string& expression);
 
 };
